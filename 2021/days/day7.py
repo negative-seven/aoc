e922eace1b9df0d07e7feb82686dd8c06b60a745
negative-seven <21268649+negative-seven@users.noreply.github.com>
2021-12-08 02:57:43 +0100

    Day 7

---
+++ 2021/days/day7.py
@@ -0,0 +1,33 @@
+from statistics import median_low
+
+from .day_template import Day
+
+
+class Day7(Day):
+	@staticmethod
+	def convert_input(raw_input):
+		return [int(n) for n in raw_input.split(',')]
+
+	@staticmethod
+	def run_first(puzzle_input):
+		target_position = median_low(puzzle_input)
+		return sum(abs(x - target_position) for x in puzzle_input)
+
+	@staticmethod
+	def run_second(puzzle_input):
+		lower_bound = min(puzzle_input)
+		upper_bound = max(puzzle_input)
+
+		while lower_bound < upper_bound - 1:
+			center = (lower_bound + upper_bound) // 2
+			center_gradient = sum(position - center - (center < position) for position in puzzle_input)
+			if center_gradient > 0:
+				lower_bound = center
+			else:
+				upper_bound = center
+
+		cost = 0
+		for position in puzzle_input:
+			distance = abs(position - lower_bound)
+			cost += distance * (distance + 1) // 2 # 1+2+3+...+n = n*(n+1)/2
+		return cost
